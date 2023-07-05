1354291769ad36ff1d7bfb26d27473e8e6b8e749
shamim <shamim@10.0.0.47>
2023-07-05 13:40:31 -0400

    First Commit

---
+++ NewProgram/src/test/java/NewProgram1.java
@@ -0,0 +1,13 @@
+
+public class NewProgram1 {
+
+	public static void main(String[] args) {
+		int a=10, b=30;
+		int Sum;
+		System.out.println("Welcome to Java");
+		Sum=a+b;
+		System.out.println("The sum is:"+Sum);
+
+	}
+
+}
